43133488537e517ab86b9025e792aa9af4b844f0
William Cotton <williamcotton@gmail.com>
2021-05-31 20:00:57 +0000

    add react-hyperscript

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import React from "react";
+import h from "react-hyperscript";
 import { renderToString } from "react-dom/server.js";
 
 const e = React.createElement;
@@ -7,7 +8,7 @@ const e = React.createElement;
 export const app = express();
 
 app.get("/", (req, res) => {
-  res.send(renderToString(e("h1", {}, "Hello, World!")));
+  res.send(renderToString(h("h1", "Hello, World!")));
 });
 
 export default app;
