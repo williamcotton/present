10af92f30547ad7f989c314c842bd384394a60c9
William Cotton <williamcotton@gmail.com>
2021-05-31 14:59:06 +0000

    add react and update feature spec to use cheerio

--- test/features/front-page.test.js
+++ test/features/front-page.test.js
@@ -1,10 +1,12 @@
 import tap from "tap";
 import fetch from "node-fetch";
+import cheerio from "cheerio";
 import { server, PORT } from "../app.js";
 
 tap.test("test", async (t) => {
   const response = await fetch(`http://localhost:${PORT}/`);
   const body = await response.text();
-  t.equal(body, "Hello, World!");
+  const $ = cheerio.load(body);
+  t.equal($("h1").text(), "Hello, World!");
   server.close();
 });
