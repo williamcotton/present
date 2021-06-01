ba0a89ebb00a4a15d94e8817d846a3a752dd38e3
William Cotton <williamcotton@gmail.com>
2021-06-01 15:44:50 +0000

    update server start

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,6 @@
-import type http from "http";
 import { app } from "./app";
 
 const PORT = 4200;
-
-let server: http.Server;
-
-async function startServer() {
-  server = app.listen(PORT);
-  console.debug(`Server running on port ${PORT}`);
-}
-
+const server = app.listen(PORT);
+console.debug(`Server running on port ${PORT}`);
 process.on("SIGTERM", () => server.close());
-
-startServer();
