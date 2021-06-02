77a80ec7c431d4a7e45d2798f60c42eecc6227e0
William Cotton <williamcotton@gmail.com>
2021-06-02 13:18:30 +0000

    add puppeteer and browser test, add .devcontainer with chromium dependencies

--- test/app.ts
+++ test/app.ts
@@ -1,6 +1,9 @@
+import path from 'path';
 import { app } from "../src/server/app";
 
 export const PORT = 4242;
+
+export const screenshotsPath = path.join(__dirname, './screenshots');
 
 export async function appFactory() {
   return app.listen(PORT);
