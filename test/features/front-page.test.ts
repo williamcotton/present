4835773c56f96d1e9d01d37e8bc3b4db49e76b57
William Cotton <williamcotton@gmail.com>
2021-06-03 14:41:26 +0000

    create test-harness and use in existing tests

--- test/features/front-page.test.ts
+++ test/features/front-page.test.ts
@@ -1,69 +1,22 @@
 import tap from "tap";
-import fetch from "node-fetch";
-import cheerio from "cheerio";
-import fs from "fs";
-import puppeteer from "puppeteer";
-import { appFactory, PORT, screenshotsPath } from "../app";
 
-const puppeteerOptions: any = {
-  args: [
-    "--no-sandbox",
-    "--disable-setuid-sandbox",
-    "--font-render-hinting=none",
-    "--use-fake-ui-for-media-stream",
-    "--use-fake-device-for-media-stream",
-    "--enable-usermedia-screen-capturing",
-    // TODO #32: --use-file-for-fake-video-capture=/home/doehlman/testvideo.y4m
-  ],
-  headless: true,
-};
-
-const chromiumPath = "/usr/bin/chromium-browser";
-if (fs.existsSync(chromiumPath)) {
-  puppeteerOptions.executablePath = chromiumPath;
-}
+import { testHarness } from "../test-harness";
 
 tap.test("[server] /", async (t) => {
-  const server = await appFactory();
-  const response = await fetch(`http://localhost:${PORT}/`);
-  const body = await response.text();
-  const $ = cheerio.load(body);
-  t.equal($("h1").text(), "Hello, World!");
-  server.close();
-});
-
-tap.test("[browser] /", async (t) => {
-  const server = await appFactory();
-  const browser = await puppeteer.launch(puppeteerOptions);
-
-  const $text = async (selector: string) =>
-    page.evaluate(
-      /* istanbul ignore next */
-      (_selector) => {
-        const element = document.querySelector(_selector);
-        return element ? element.innerText : false;
-      },
-      selector
-    );
-
-  const page = await browser.newPage();
-
-  await page.setViewport({
-    width: 1200,
-    height: 1080,
-  });
-
-  await page.goto(`http://localhost:${PORT}/`);
-
-  await page.waitForSelector('h1');
-  await page.screenshot({
-    path: `${screenshotsPath}/front-page.png`,
-    fullPage: true,
-  });
+  const { close, get$ } = await testHarness("server");
+  const { $text } = await get$("/");
 
   t.equal(await $text("h1"), "Hello, World!");
 
-  await page.close();
-  await browser.close();
-  server.close();
+  close();
+});
+
+tap.test("[browser] /", async (t) => {
+  const { close, get$ } = await testHarness();
+  const { $text, screenshot } = await get$("/");
+
+  t.equal(await $text("h1"), "Hello, World!");
+  await screenshot("front-page");
+
+  close();
 });
---
+++ test/test-harness.ts
@@ -0,0 +1,152 @@
+import path from "path";
+import puppeteer from "puppeteer";
+import fs from "fs";
+import fetch from "node-fetch";
+import cheerio from "cheerio";
+import { app } from "../src/server/app";
+
+const puppeteerOptions: any = {
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--font-render-hinting=none",
+    "--use-fake-ui-for-media-stream",
+    "--use-fake-device-for-media-stream",
+    "--enable-usermedia-screen-capturing",
+    // TODO #32: --use-file-for-fake-video-capture=/home/doehlman/testvideo.y4m
+  ],
+  headless: true,
+};
+
+const chromiumPath = "/usr/bin/chromium-browser";
+if (fs.existsSync(chromiumPath)) {
+  puppeteerOptions.executablePath = chromiumPath;
+}
+
+export const PORT = 4242;
+
+export const baseUrl = `http://localhost:${PORT}`;
+
+export const screenshotsPath = path.join(__dirname, "./screenshots");
+
+async function browserHarness() {
+  const browser = await puppeteer.launch(puppeteerOptions);
+
+  const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 1200,
+    height: 1080,
+  });
+
+  const server = app.listen(PORT);
+
+  const close = () => {
+    page.close();
+    browser.close();
+    server.close();
+  };
+
+  const get$ = async (path: string) => {
+    const url = baseUrl + path;
+    await page.goto(url);
+
+    const $text = async (selector: string) => {
+      await page.waitForSelector(selector);
+      return page.evaluate(
+        /* istanbul ignore next */
+        (_selector) => {
+          const element = document.querySelector(_selector);
+          return element ? element.innerText : false;
+        },
+        selector
+      );
+    };
+
+    const $html = async (selector: string) =>
+      page.evaluate(
+        /* istanbul ignore next */
+        (_selector) => {
+          const element = document.querySelector(_selector);
+          return element ? element.innerHTML : false;
+        },
+        selector
+      );
+
+    const currentRoute = () => page.url().split(baseUrl)[1];
+
+    const findByText = async (text: string) =>
+      page.$x(`//*[contains(., '${text}')]/@*`);
+
+    const getAttr = async (handle: any) => {
+      const propertyHandle = await handle.getProperty("value");
+      const attr = await propertyHandle.jsonValue();
+      return attr;
+    };
+
+    const screenshot = async (name: string) => {
+      await page.screenshot({
+        path: `${screenshotsPath}/${name}.png`,
+        fullPage: true,
+      });
+    };
+
+    return {
+      $text,
+      currentRoute,
+      page,
+      findByText,
+      getAttr,
+      baseUrl,
+      $html,
+      browser,
+      screenshot,
+    };
+  };
+
+  return { close, get$ };
+}
+
+async function serverHarness() {
+  const server = app.listen(PORT);
+
+  const close = () => {
+    server.close();
+  };
+
+  const get$ = async (path: string) => {
+    const url = baseUrl + path;
+    const response = await fetch(url);
+    const body = await response.text();
+    const $ = cheerio.load(body);
+    const $text = async (selector: string) => $(selector).text();
+    return { $text, screenshot: async () => {} };
+  };
+
+  return { close, get$ };
+}
+
+export async function testHarness(
+  environment: "browser" | "server" = "browser"
+): Promise<{
+  close: () => void;
+  get$: (path: string) => Promise<{
+    $text: (selector: string) => Promise<string>;
+    screenshot: (name: string) => Promise<any>;
+    currentRoute?: () => string;
+    page?: any;
+    findByText?: any;
+    getAttr?: (handle: any) => Promise<{}>;
+    baseUrl?: string;
+    $html?: (selector: string) => Promise<string | false>;
+    browser?: any;
+  }>;
+}> {
+  if (environment === "browser") {
+    return browserHarness();
+  } else {
+    return serverHarness();
+  }
+}
+
+export default testHarness;
